54993bebc356788e6cf4798c149bf3f905835550
giannis-dallas <giannisdallas81@hotmail.com>
2019-10-26 01:30:59 +0300

    allow cors

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const port = process.env.PORT || 3000
 
 const graduates = require('./assets/graduates.json')
 
+app.all('/data', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.get('/', (req, res) => res.send('Hey there!') )
 app.get('/data', (req, res) => res.json(graduates) )
 
