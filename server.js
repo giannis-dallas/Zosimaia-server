62053e33040ed0fb0742ffc3f31fb5825dd6cc36
giannis-dallas <giannisdallas81@hotmail.com>
2019-11-06 00:33:17 +0200

    deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const mysql = require('mysql');
 
 let mainData;
-//let testData;
+let testData;
 
 const graduates = require('./assets/graduates.json')
 
@@ -12,17 +12,23 @@ const connection = mysql.createConnection({
   host     : '31.22.113.24',
   user     : 'zosimaia_dbm',
   password : '#4m7l*%XPfGX',
-  database : 'zosimaia_db'
+  database : 'zosimaia_db',
+  port: 3306
 });
 connection.connect();
 connection.query('SELECT Name,Surname,YearOf,HomeAdCity_ID,Job_ID FROM graduates', (err,rows) => {
-    if(err){ console.log(err)};
+    if(err){
+      console.log(err)
+      throw err
+    };
 
     console.log('Data received from Db:');
     console.log(rows[0].Name);
-    //testData=rows[0].Name;
+    testData=rows[0].Name;
     mainData=rows;
 });
+
+connection.end(); 
 
 app.all('/data', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -31,7 +37,7 @@ app.all('/data', function(req, res, next) {
 });
 
 app.get('/', (req, res) => res.send('Hey there!') )
-//app.get('/test', (req, res) => res.send('Random test! '+ testData) )
+app.get('/test', (req, res) => res.send('Random test! '+ testData) )
 app.get('/data', (req, res) => res.json(mainData) )
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
