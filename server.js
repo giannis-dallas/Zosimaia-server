3cdeeb3c5be95ca1ddae7eb61dcb00359b4ad936
giannis-dallas <giannisdallas81@hotmail.com>
2019-11-07 19:41:43 +0200

    final

--- server.js
+++ server.js
@@ -3,32 +3,18 @@ const app = express()
 const port = process.env.PORT || 3000
 const mysql = require('mysql');
 
-let mainData;
-let testData;
-
-const graduates = require('./assets/graduates.json')
+var mainData;
+let citiesData;
+let jobsData;
 
 const connection = mysql.createConnection({
   host     : '31.22.113.24',
   user     : 'zosimaia_dbm',
   password : '#4m7l*%XPfGX',
   database : 'zosimaia_db',
-  port: 3306
+  multipleStatements: true
 });
 connection.connect();
-connection.query('SELECT Name,Surname,YearOf,HomeAdCity_ID,Job_ID FROM graduates', (err,rows) => {
-    if(err){
-      console.log(err)
-      throw err
-    };
-
-    console.log('Data received from Db:');
-    console.log(rows[0].Name);
-    testData=rows[0].Name;
-    mainData=rows;
-});
-
-connection.end(); 
 
 app.all('/data', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -38,6 +24,26 @@ app.all('/data', function(req, res, next) {
 
 app.get('/', (req, res) => res.send('Hey there!') )
 app.get('/test', (req, res) => res.send('Random test! '+ testData) )
-app.get('/data', (req, res) => res.json(mainData) )
+app.get('/data', (req, res) => {
+
+  connection.query(`SELECT Name,Surname,YearOf,HomeAdCity_ID,Job_ID FROM graduates;
+    SELECT id,City_Name FROM cities;
+    SELECT id,Name FROM jobs`
+    , (err,rows) => {
+        if (err) throw err ;
+        mainData=rows[0];
+        citiesData=rows[1];
+        jobsData=rows[2];
+        result = mainData.map( (graduate) => ({
+              ...graduate,
+              momeCity: graduate.HomeAdCity_ID ? citiesData.find(city => city.id == graduate.HomeAdCity_ID).City_Name : '',
+              job : graduate.Job_ID ? jobsData.find(job => job.id == graduate.Job_ID).Name : ''
+            })
+          )
+    console.log(result);
+    res.json(result) 
+  })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
